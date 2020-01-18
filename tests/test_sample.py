812ffb4c91f2012c03571179a30f982f4a61154a
Loïc Faure-Lacroix <lamerstar@gmail.com>
2020-01-18 14:55:00 -0500

    Added dotted syntax test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from prometheus_sd.utils import (
+    extract_prometheus_labels,
+    dotted_setter,
+    convert_labels_to_config,
+    labels_prefix
+)
 
 def f():
     raise Exception("test")
@@ -9,3 +15,18 @@ def f():
 def test_something():
     with pytest.raises(Exception):
         f()
+
+def test_good_labels(self):
+    labels = {
+        "a.b.c": 1,
+        "a.b.c.d": 2,
+        "prometheus.jobs.fun": 3,
+        "prometheus.enable": 4
+    }
+
+    new_labels = extract_prometheus_labels(labels)
+
+    assert len(new_labels) == 2
+
+    for key in new_labels.keys():
+        assert key.startswith(labels_prefix) == True
