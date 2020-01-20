c9086361b501d5d2a178370197fab4bf15d72e00
Loïc Faure-Lacroix <lamerstar@gmail.com>
2020-01-20 10:35:59 -0500

    Moved tests inside prometheus_sd for sharing tests classes

--- prometheus_sd/tests/test_web.py
+++ prometheus_sd/tests/test_web.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
+import aiohttp
 from prometheus_sd.server import (
     make_app,
     make_server
 )
 from prometheus_sd.config import Config, get_parser
+from prometheus_sd.tests.test_config import DockerClientMock
 
 async def test_app_metrics(aiohttp_client, loop):
     parser = get_parser()
@@ -35,3 +37,22 @@ async def test_app_metrics(aiohttp_client, loop):
     assert 'promsd_build_seconds' in text
     assert 'promsd_config_size_bytes' in text
     assert 'promsd_configs_units' in text
+
+
+async def test_server_metrics(aiohttp_client, loop):
+    parser = get_parser()
+
+    args = [
+        '--out', 'text.json',
+        '--metrics',
+    ]
+
+    config = Config(parser, args, DockerClientMock)
+
+    assert config.options.metrics == True
+
+    webserver = loop.create_task(make_server(config))
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://localhost:9090/metrics') as resp:
+            assert resp.status == 200
